f8ae2cc57e74176c1fd19d8abf553356472a9d23
marcel <marcelinseattle@gmail.com>
2017-04-07 12:24:10 -0700

    Add a killing bug device to the pizza project

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+//Business Logic
+
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+}
+
+Pizza.prototype.pizzaCost = function() {
+  var cost = 0;
+
+  if (this.topping === "topping1"){
+    cost += 1;
+  } else if (this.topping === "topping2") {
+    cost += 2;
+  } else if (this.topping === "topping3") {
+    cost += 3;
+  }
+
+  if (this.size === "small") {
+    cost += 5;
+  } else if (this.size === "medium") {
+    cost += 10;
+  } else if (this.size === "large") {
+    cost += 15;
+  }
+  return cost;
+}
+
+
+
+
+//User Logic
+$(document).ready(function(){
+  $("#pizza").submit(function(event){
+    event.preventDefault();
+    var nameInput = $("#name").val();
+    var toppingInput = $("#topping").val();
+    var sizeInput = $("#size").val();
+
+    var newPizza = new Pizza(toppingInput, sizeInput);
+
+    console.log(newPizza.pizzaCost());
+    $(".cost-show").show();
+
+
+    $(".name").text(nameInput);
+    $(".price").text(newPizza.pizzaCost());
+
+    $("#name").val("");
+
+  });
+});
